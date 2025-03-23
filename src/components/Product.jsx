e5413983e7321e7c57eccba665edfbfe40550f5a
dostonbek <dostonbek@gmail.com>
2025-03-23 11:32:38 +0500

    Refactor responsive image sizes and add border to product image

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,9 +10,9 @@ function Product({ d }) {
   return (
     <div>
       <picture>
-        <source srcSet={d.image.mobile} media="(max-width: 500px)" />
-        <source srcSet={d.image.tablet} media="(max-width: 850px)" />
-        <source srcSet={d.image.desktop} media="(min-width: 851px)" />
+        <source srcSet={d.image.mobile} media="(max-width: 450px)" />
+        <source srcSet={d.image.tablet} media="(max-width: 800px)" />
+        <source srcSet={d.image.desktop} media="(min-width: 801px)" />
 
         <button
           className={`card ${isAdded ? "added" : ""}`}
@@ -49,8 +49,11 @@ function Product({ d }) {
             </>
           )}
         </button>
-
-        <img src={d.image.desktop} alt={d.name} className="product-image" />
+        <img
+          src={d.image.desktop}
+          alt={d.name}
+          className={`product-image ${isAdded ? "border-red" : ""}`}
+        />
       </picture>
       <h3 className="categorys">{d.category}</h3>
       <h2 className="names">{d.name}</h2>
